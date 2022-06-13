d69e3a92b5ed088e37c310233b16fb25344056e1
Lakshmi Shreya B.Y <76276531+Lakshmi-Shreya@users.noreply.github.com>
2022-06-13 21:46:24 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      {" "}
+      <p>Made With ❤ By Lakshmi Shreya</p>
+    </div>
+  );
+}
+
+export default Footer;
